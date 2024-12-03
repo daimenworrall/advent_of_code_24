ae309fd7a6c125588f715983e2694bc1f86c9fb2
Daimen Worrall <daimen@worrall.pw>
2024-12-03 23:41:58 +0000

    challenge 2 part 2 done

---
+++ src/day_two/part_two/index.test.ts
@@ -0,0 +1,10 @@
+import { main } from ".";
+
+describe("Challenge 2", () => {
+    it("should do a thing", async () => {
+        //LITERALLY JUST TO EASILY RUN THE CODE
+        //DO NOT JUDGE ME LEST YE BE JUDGED YOURSELF
+        await main();
+        expect(true).toBe(true);
+    });
+});
